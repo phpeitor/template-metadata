dce39a986bf5c86bb71473c7c8152d4b050eb3ff
FORTELalejandro.montalvan <alejandro.montalvan@fortelcorp.com>
2025-09-01 00:08:49 -0500

    contacto commit

--- js/jquery-plugin-collection_.js
+++ js/jquery-plugin-collection_.js
@@ -791,3 +791,20 @@ window.onload = function(){
 	var ctx = document.getElementById("barChart").getContext("2d");
 	window.myBar = new Chart(ctx).Bar(barChartData, { responsive: true });
 };
+
+$('#mailchimp-subscription-form2').ajaxChimp({
+	callback: mailChimpCallBack,
+	url: '//thememascot.us9.list-manage.com/subscribe/post?u=a01f440178e35febc8cf4e51f&amp;id=49d6d30e1e'
+});
+
+function mailChimpCallBack(resp) {
+	var $mailchimpform = $('#mailchimp-subscription-form2'),
+		$response = '';
+	$mailchimpform.children(".alert").remove();
+	if (resp.result === 'success') {
+		$response = '<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + resp.msg + '</div>';
+	} else if (resp.result === 'error') {
+		$response = '<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + resp.msg + '</div>';
+	}
+	$mailchimpform.prepend($response);
+}
